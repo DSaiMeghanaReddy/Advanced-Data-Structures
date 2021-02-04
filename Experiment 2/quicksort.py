2b362fa8c832df1cdd3aa697eb282e99d9bf88cd
meghana67 <77834002+meghana67@users.noreply.github.com>
2021-02-04 15:48:09 +0530

    Update quicksort.py

--- Experiment 2/quicksort.py
+++ Experiment 2/quicksort.py
@@ -1,21 +1,31 @@
-def partition(arr,low,high):
-   i = ( low-1 )
-   pivot = arr[high] 
-   for j in range(low , high):
-      if arr[j] <= pivot:
-         # increment
-         i = i+1
-         arr[i],arr[j] = arr[j],arr[i]
-   arr[i+1],arr[high] = arr[high],arr[i+1]
-   return ( i+1 )
-def quickSort(arr,low,high):
-   if low < high:
-      pi = partition(arr,low,high)
-      quickSort(arr, low, pi-1)
-      quickSort(arr, pi+1, high)
-arr = [2,5,3,8,6,5,4,7]
-n = len(arr)
-quickSort(arr,0,n-1)
-print ("Sorted array is:")
-for i in range(n):
-   print (arr[i],end=" ")
+def quicksort(arr, begin, end):
+    
+    if end - begin > 1:
+        p = partition(arr, begin, end)
+        quicksort(arr, begin, p)
+        quicksort(arr, p + 1, end)
+ 
+ 
+def partition(arr, begin, end):
+    pivot = arr[begin]
+    i = begin + 1
+    j = end - 1
+ 
+    while True:
+        while (i <= j and arr[i] <= pivot):
+            i = i + 1
+        while (i <= j and arr[j] >= pivot):
+            j = j - 1
+ 
+        if i <= j:
+           arr[i], arr[j] = arr[j], arr[i]
+        else:
+            arr[begin], arr[j] = arr[j], arr[begin]
+            return j
+ 
+ 
+arr = input('Enter the list of numbers to be Sorted: ').split()
+arr = [int(x) for x in arr]
+quicksort(arr, 0, len(arr))
+print('Sorted list: ', end='')
+print(arr)
