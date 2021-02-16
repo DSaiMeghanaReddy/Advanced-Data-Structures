8fe6d73a027405157b2723cedfc46f8e2c27da0f
D.SaiMeghana Reddy <77834002+DSaiMeghanaReddy@users.noreply.github.com>
2021-02-16 15:23:45 +0530

    Create inser_an_element_in_dictionary.py

---
+++ Experiment 9a/inser_an_element_in_dictionary.py
@@ -0,0 +1,7 @@
+def insert(key,val):
+    myDict[key] = val
+    print(myDict)
+myDict = {'java':100,'python':20,'c':300, 20:22, 32:42}
+insert('cplus',90)
+insert('dbms',80)
+insert('machinelanguage',200)
